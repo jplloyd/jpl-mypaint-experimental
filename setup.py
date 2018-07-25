9aee502fbbbbe8fedffb48ced978c9af7b9275d1
Jesper Lloyd <jpl.lloyd@gmail.com>
2018-07-25 21:55:04 +0200

    Add dilation/erosion feature to flood fill
    Add Urbach-Wilkinson morphological operations implemented in C++.
    Add local search to enable skipping tiles for large-radius ops.
    
    Add functions in lib.floodfill to handle the morph input/output tiles
    and call the lower-level operations.
    
    Add conditional multiprocess distribution of heavy workloads.
    Update flood fill call chain and param docs.
    Add spinner to flood fill GUI
    
    closes #73

--- setup.py
+++ setup.py
@@ -525,6 +525,7 @@ def get_ext_modules():
         [
             'lib/mypaintlib.i',
             'lib/floodfill.cpp',
+            'lib/morphology.cpp',
             'lib/gdkpixbuf2numpy.cpp',
             'lib/pixops.cpp',
             'lib/fastpng.cpp',
