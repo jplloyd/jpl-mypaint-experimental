jplloyd/jpl-mypaint-experimental

commit cb69f7531b6ef0aeb8f9c21033a2f192322b9f29

Add drag-fill functionality to the fill tool
Add the ability to drag-select a range of starting
seeds that the fill is run on, instead of a single
initial seed (still possible; a single click works
the same as before).

The target color is based on the starting position
of the drag.

New overlay indicates the seed pixels when dragging.

Update call chains and the fill functions to handle
seed partitioning and sizing initial bounding boxes.

Update test code.

Add new cursor properties to work with DragMode.
Factor out cursor update from general ui update.

lib/floodfill.py
@@ -92,6 +92,22 @@ def starting_coordinates(x, y):
     return init_tx, init_ty, init_x, init_y
 
 
+def seeds_by_tile(seeds):
+    """Partition and convert seed coordinates
+
+    Partition a list of model-space seed coordinates into lists of
+    in-tile coordinates associated to their respective tile coordinate
+    in a dictionary.
+    """
+    tile_seeds = dict()
+    for (x, y) in seeds:
+        tx, ty, px, py = starting_coordinates(x, y)
+        seed_list = tile_seeds.get((tx, ty), [])
+        seed_list.append((px, py))
+        tile_seeds[(tx, ty)] = seed_list
+    return tile_seeds
+
+
 def get_target_color(src, tx, ty, px, py):
     """Get the pixel color for the given tile/pixel coordinates"""
     with src.tile_request(tx, ty, readonly=True) as start:
@@ -107,14 +123,16 @@ def get_target_color(src, tx, ty, px, py):
 # Main fill handling function
 
 def flood_fill(
-        src, x, y, color, tolerance, offset, feather,
+        src, target_pos, seeds, color, tolerance, offset, feather,
         gap_closing_options, mode, framed, bbox, dst):
     """ Top-level flood fill interface, initiating and delegating actual fill
 
     :param src: Source surface-like object
     :type src: Anything supporting readonly tile_request()
-    :param x: Starting point X coordinate
-    :param y: Starting point Y coordinate
+    :param target_pos: pixel coordinate of target color
+    :type target_pos: tuple
+    :param seeds: set of seed pixel coordinates {(x, y)...}
+    :type seeds: set
     :param color: an RGB color
     :type color: tuple
     :param tolerance: how much filled pixels are permitted to vary
@@ -149,11 +167,12 @@ def flood_fill(
     feather = lib.helpers.clamp(feather, 0, TILE_SIZE)
 
     # Initial parameters
-    starting_point = starting_coordinates(x, y)
+    starting_point = starting_coordinates(*target_pos)
     r, g, b, a = get_target_color(src, *starting_point)
     filler = myplib.Filler(r, g, b, a, tolerance)
+    seed_lists = seeds_by_tile(seeds)
 
-    fill_args = (src, starting_point, tiles_bbox, filler)
+    fill_args = (src, seed_lists, tiles_bbox, filler)
 
     if gap_closing_options:
         fill_args += (gap_closing_options,)
@@ -255,7 +274,7 @@ def composite(mode, fill_col, trim_result, filled, tiles_bbox, dst):
         dst.notify_observers(*bbox)
 
 
-def scanline_fill(src, init, tiles_bbox, filler):
+def scanline_fill(src, seed_lists, tiles_bbox, filler):
     """ Perform a scanline fill and return the filled tiles
 
     Perform a scanline fill using the given starting point and tile,
@@ -263,6 +282,8 @@ def scanline_fill(src, init, tiles_bbox, filler):
     provided filler instance.
 
     :param src: Source surface-like object
+    :param seed_lists: dictionary, pairing tile coords with lists of seeds
+    :type seed_lists: dict
     :param init: coordinates for starting tile and pixel
     :type init: (int, int, int, int)
     :param tiles_bbox: Bounding box for the fill
@@ -283,8 +304,10 @@ def scanline_fill(src, init, tiles_bbox, filler):
     )
 
     # Starting coordinates + direction of origin (from within)
-    _tx, _ty, _px, _py = init
-    tileq = [((_tx, _ty), (_px, _py), myplib.edges.none)]
+    tileq = []
+    pairs = seed_lists.items() if PY3 else seed_lists.iteritems()
+    for tc, seeds in pairs:
+        tileq.append((tc, seeds, myplib.edges.none))
 
     tfs = _TileFillSkipper(tiles_bbox, filler, set({}))
 
@@ -374,7 +397,7 @@ class _TileFillSkipper:
         return self.FULL_OVERFLOWS[from_dir]
 
 
-def gap_closing_fill(src, init, tiles_bbox, filler, gap_closing_options):
+def gap_closing_fill(src, seed_lists, tiles_bbox, filler, gap_closing_options):
     """ Fill loop that finds and uses gap data to avoid unwanted leaks
 
     Gaps are defined as distances of fillable pixels enclosed on two sides
@@ -398,21 +421,24 @@ def gap_closing_fill(src, init, tiles_bbox, filler, gap_closing_options):
         [(edge.south,), (edge.west,), (edge.north,), (edge.east,)],
     ]
 
+    tileq = []
+    pairs = seed_lists.items() if PY3 else seed_lists.iteritems()
+    for tc, seeds in pairs:
+        tileq.append((tc, seeds))
+
     options = gap_closing_options
     max_gap_size = lib.helpers.clamp(options.max_gap_size, 1, TILE_SIZE)
     gc_filler = myplib.GapClosingFiller(max_gap_size, options.retract_seeps)
     distbucket = myplib.DistanceBucket(max_gap_size)
 
-    init_tx, init_ty, init_px, init_py = init
-    tileq = [((init_tx, init_ty), (init_px, init_py))]
-
     total_px = 0
-
+    tiles_processed = 0
     dist_data = None
     while len(tileq) > 0:
         tile_coord, seeds = tileq.pop(0)
         if tile_coord in final:
             continue
+        tiles_processed += 1
         # Pixel limits within tiles vary at the bounding box edges
         px_bounds = tiles_bbox.tile_bounds(tile_coord)
         # Create distance-data and alpha output tiles for the fill
@@ -423,20 +449,18 @@ def gap_closing_fill(src, init, tiles_bbox, filler, gap_closing_options):
             if dist_data is None:
                 dist_data = np.full((N, N), 2*N*N, 'uint16')
             # Search and mark any gap distances for the tile
-            if (
-                    all(map(lambda t: t is _FULL_TILE, grid))
-                    or not myplib.find_gaps(distbucket, dist_data, *grid)
-            ):
+            # We skip the search if we have a 9-grid of only full tiles
+            # since there cannot be any gaps to find in that case
+            all_full = all(map(lambda t: t is _FULL_TILE, grid))
+            if all_full or not myplib.find_gaps(distbucket, dist_data, *grid):
                 distances[tile_coord] = _GAPLESS_TILE
                 # Check if fill can be skipped directly
-                if (
-                        grid[0] is _FULL_TILE and
-                        not tiles_bbox.crossing(tile_coord) and
-                        isinstance(seeds, tuple)
-                ):
+                can_skip = isinstance(seeds, tuple) or len(seeds[0]) == 2
+                can_skip &= not tiles_bbox.crossing(tile_coord)
+                if can_skip and grid[0] is _FULL_TILE:
                     final.add(tile_coord)
                     filled[tile_coord] = _FULL_TILE
-                    if len(seeds) > 1:
+                    if isinstance(seeds, list):
                         out_seeds = const_overflows[edge.none]
                     else:
                         out_seeds = const_overflows[seeds[0]]
@@ -446,20 +470,26 @@ def gap_closing_fill(src, init, tiles_bbox, filler, gap_closing_options):
                 distances[tile_coord] = dist_data
                 dist_data = None
             filled[tile_coord] = np.zeros((N, N), 'uint16')
-        if isinstance(seeds, tuple) and len(seeds) > 1:
+        # Complement data for initial seeds
+        if not isinstance(seeds, tuple) and len(seeds[0]) < 3:
             # Fetch distance for initial seed coord
+            complemented_seeds = []
             dists = distances[tile_coord]
-            init_x, init_y = seeds
-            init_distance = dists[init_y][init_x]
+            all_not_max = True
+            for (px, py) in seeds:
+                distance = dists[px][py]
+                if distance == 2*N*N:
+                    all_not_max = False
+                complemented_seeds.append((px, py, distance))
+            seeds = complemented_seeds
             # If the fill is starting at a point with a detected distance,
             # disable seep retraction - otherwise it is very likely
             # that the result will be completely empty.
-            if init_distance < 2*N*N:
+            if len(seed_lists) == 1 and all_not_max:
                 options.retract_seeps = False
                 gc_filler = myplib.GapClosingFiller(
                     max_gap_size, options.retract_seeps
                 )
-            seeds = [(init_x, init_y, init_distance)]
         # Run the gap-closing fill for the tile
         result = gc_filler.fill(
             full_alphas[tile_coord], distances[tile_coord],
@@ -475,7 +505,6 @@ def gap_closing_fill(src, init, tiles_bbox, filler, gap_closing_options):
         total_px += px_f
         if fill_edges:
             unseep_q.append((tile_coord, fill_edges, True))
-
     # Seep inversion is basically just a four-way 0-alpha fill
     # with different conditions. It only backs off into the original
     # fill and therefore does not require creation of new tiles

gui/fill.py
@@ -19,26 +19,30 @@ from gi.repository import Pango
 from gettext import gettext as _
 from lib.gettext import C_
 
+import cairo
+
 import gui.mode
 import gui.cursor
 import gui.blendmodehandler
+import gui.overlays
 
 import lib.floodfill
 import lib.helpers
 import lib.mypaintlib
 import lib.layer
 
-
 ## Class defs
 
 class FloodFillMode (gui.mode.ScrollableModeMixin,
-                     gui.mode.SingleClickMode):
+                     gui.mode.DragMode):
     """Mode for flood-filling with the current brush color"""
 
     ## Class constants
 
     ACTION_NAME = "FloodFillMode"
     GC_ACTION_NAME = "FloodFillGCMode"
+
+    SPRING_LOADED = False
 
     permitted_switch_actions = set([
         'RotateViewMode', 'ZoomViewMode', 'PanViewMode',
@@ -62,6 +66,14 @@ class FloodFillMode (gui.mode.ScrollableModeMixin,
     _fill_permitted = True
     _x = None
     _y = None
+
+    @property
+    def active_cursor(self):
+        return self.cursor
+
+    @property
+    def inactive_cursor(self):
+        return self.cursor
 
     @property
     def cursor(self):
@@ -111,28 +123,53 @@ class FloodFillMode (gui.mode.ScrollableModeMixin,
         from gui.application import get_app
         self.app = get_app()
         self.bm = self.get_blend_modes()
-        self.bm.mode_changed += self.update_blendmode
+        self.bm.mode_changed += self.update_blend_mode
         self._prev_release = (0, None)
+        self._seed_pixels = set()
+        self._overlay = None
+        self._target_pos = None
 
-    def update_blendmode(self, bm, old, new):
-        if(old is not new):
-            self._update_ui()
+    def update_blend_mode(self, mode_manager, old_mode, new_mode):
+        if old_mode is not new_mode:
+            self._update_cursor(self.get_options_widget())
 
-    def clicked_cb(self, tdw, event):
-        """Flood-fill with the current settings where clicked
+    def drag_update_cb(self, tdw, event, dx, dy):
+        """Add pixel coordinate to seed set (if not there already)"""
+        x, y = tdw.display_to_model(event.x, event.y)
+        seed_candidate = (int(x), int(y))
+        self._overlay.add_point((event.x, event.y))
+        if self._fill_permitted and seed_candidate not in self._seed_pixels:
+            self._seed_pixels.add(seed_candidate)
+
+    def drag_start_cb(self, tdw, event):
+        """Create overlay and set initial target pixel & seed"""
+        self._seed_pixels = set()
+        x, y = tdw.display_to_model(event.x, event.y)
+        # Permit setting the target color (the one pixels are tested against)
+        # from outside a frame, but no corresponding seed is added
+        self._target_pos = (x, y)
+        self._overlay = FloodFillOverlay(tdw)
+        self._overlay.add_point((event.x, event.y))
+        if self._fill_permitted:
+            self._seed_pixels.add((x, y))
+
+    def drag_stop_cb(self, tdw):
+        """Remove overlay and run the fill if valid seeds were marked"""
+        self._overlay.cleanup()
+        self._overlay = None
+        if self._seed_pixels:
+            self.fill(tdw)
+
+    def fill(self, tdw):
+        """Flood-fill with the current settings and marked seeds
 
         If the current layer is not fillable, a new layer will always be
         created for the fill.
         """
-        if not self._fill_permitted:
-            return
         try:
             self.EOTF = self.app.preferences['display.colorspace_EOTF']
         except: 
             self.EOTF = 2.2
-        x, y = tdw.display_to_model(event.x, event.y)
-        self._x = x
-        self._y = y
         self._tdws.add(tdw)
         self._update_ui()
         color = self.doc.app.brush_color_manager.get_color()
@@ -147,7 +184,9 @@ class FloodFillMode (gui.mode.ScrollableModeMixin,
         if opts.limit_to_view:
             corners = tdw.get_corners_model_coords()
             view_bbox = lib.helpers.rotated_rectangle_bbox(corners)
-        tdw.doc.flood_fill(x, y, rgb,
+        seeds = self._seed_pixels
+        target_pos = self._target_pos
+        tdw.doc.flood_fill(target_pos, seeds, rgb,
                            tolerance=opts.tolerance,
                            view_bbox=view_bbox,
                            offset=opts.offset, feather=opts.feather,
@@ -203,9 +242,15 @@ class FloodFillMode (gui.mode.ScrollableModeMixin,
         if permitted and model.frame_enabled:
             fx1, fy1, fw, fh = model.get_frame()
             fx2, fy2 = fx1+fw, fy1+fh
-            permitted = x >= fx1 and y >= fy1 and x < fx2 and y < fy2
+            permitted = fx1 <= x < fx2 and fy1 <= y < fy2
         self._fill_permitted = permitted
+        self._update_cursor(opts)
 
+    def _update_cursor(self, opts):
+        """Update the cursor
+        The cursor used is based on fill options, blend mode,
+        and if a fill can be run for the current position and layer.
+        """
         # Update cursor of any TDWs we've crossed
         if self._fill_permitted:
             cursor = (opts.gap_closing, self.get_current_cursor())
@@ -239,6 +284,81 @@ class FloodFillMode (gui.mode.ScrollableModeMixin,
             widget = FloodFillOptionsWidget()
             cls._OPTIONS_WIDGET = widget
         return cls._OPTIONS_WIDGET
+
+
+class FloodFillOverlay (gui.overlays.Overlay):
+    """
+    Overlay indicating pixels that constitute the fill seeds
+
+    The line width is zoom independent, so the visualization
+    does not actually correspond 1:1 to the seed pixels.
+    Marking pixel locations directly however, would make
+    the markings virtually invisible when zoomed out.
+    """
+
+    DASH_LENGTH = 9
+    # Clean out (SKIP-1)/SKIP points for smoother lines
+    SKIP = 6
+
+    def __init__(self, tdw):
+        self._tdw = tdw
+        self._line_points = []
+        self._skip_index = 0
+        self._full_bounds = None
+        tdw.display_overlays.append(self)
+
+    def cleanup(self):
+        self._line_points = []
+        self._tdw.display_overlays.remove(self)
+        self._skip_index = 0
+        self._tdw.queue_draw_area(*self._full_bounds)
+
+    def add_point(self, point):
+        x, y = point
+        self._full_bounds = self._update_bounds(self._full_bounds, x, y)
+        # Clean up lines by only storing every nth point (n = SKIP)
+        if len(self._line_points) > 1 and self._skip_index % self.SKIP != 0:
+            self._line_points[len(self._line_points) - 1] = point
+        else:
+            self._line_points.append(point)
+        self._skip_index += 1
+        self._tdw.queue_draw_area(*self._full_bounds)
+
+    @staticmethod
+    def _update_bounds(bounds, x, y):
+        if bounds is None:
+            return x, y, 1, 1
+        else:
+            bx, by, w, h = bounds
+            if x < bx:
+                w += (bx - x)
+                bx = x
+            elif x > bx:
+                w += (x - bx)
+            if y < by:
+                h += (by - y)
+                by = y
+            elif y > by:
+                h += (y - by)
+            return bx, by, w, h
+
+    def paint(self, cr):
+        """
+        Paint the pixels of the marked set (approximately) as a stroke
+        with alternating white and black dashes, for visibility
+        """
+        d_len = self.DASH_LENGTH
+        if len(self._line_points) > 1:
+            cr.set_line_join(cairo.LINE_JOIN_ROUND)
+            cr.set_source_rgba(0.0, 0.0, 0.0, 0.8)
+            cr.set_dash([d_len])
+            cr.move_to(*self._line_points[0])
+            for (x, y) in self._line_points[1:]:
+                cr.line_to(x, y)
+            cr.stroke_preserve()
+            cr.set_source_rgba(1.0, 1.0, 1.0, 0.8)
+            cr.set_dash([d_len], d_len)
+            cr.stroke()
 
 
 class FloodFillOptionsWidget (Gtk.Grid):

lib/command.py
@@ -509,12 +509,12 @@ class FloodFill (Command):
 
     display_name = _("Flood Fill")
 
-    def __init__(self, doc, x, y, color, tolerance, offset, feather,
-                 gap_closing_options, mode, bbox,
+    def __init__(self, doc, target_pos, seeds, color, tolerance,
+                 offset, feather, gap_closing_options, mode, bbox,
                  sample_merged, src_path, make_new_layer, **kwds):
         super(FloodFill, self).__init__(doc, **kwds)
-        self.x = x
-        self.y = y
+        self.target_pos = target_pos
+        self.seeds = seeds
         self.color = color
         self.tolerance = tolerance
         self.offset = offset
@@ -562,7 +562,7 @@ class FloodFill (Command):
             self.snapshot = layers.current.save_snapshot()
             dst_layer = layers.current
         # Fill connected areas of the source into the destination
-        fill_args = (self.x, self.y, self.color, self.tolerance,
+        fill_args = (self.target_pos, self.seeds, self.color, self.tolerance,
                      self.offset, self.feather, self.gap_closing_options,
                      self.mode, self.framed, self.bbox)
         src_layer.flood_fill(*fill_args, dst_layer=dst_layer)

lib/document.py
@@ -993,17 +993,22 @@ class Document (object):
 
     ## Other painting/drawing
 
-    def flood_fill(self, x, y, color, tolerance=0.1, view_bbox=None,
+    def flood_fill(self, target_pos, seeds, color, tolerance=0.1,
+                   view_bbox=None,
                    offset=0, feather=0, gap_closing_options=None, mode=0,
                    sample_merged=False, src_path=None, make_new_layer=False):
         """Flood-fills a point on the current layer with a color
 
-        :param x: Starting point X coordinate
-        :param y: Starting point Y coordinate
+        :param target_pos: pixel coordinate of target color
+        :type target_pos: tuple
+        :param seeds: set of seed pixel coordinates {(x, y)...}
+        :type seeds: set
         :param color: The RGB color to fill connected pixels with
         :type color: tuple
         :param tolerance: How much filled pixels are permitted to vary
         :type tolerance: float [0.0, 1.0]
+        :param view_bbox: Bounding box of the view, restricts fill if present
+        :type view_bbox: lib.helpers.Rect
         :param offset: the post-fill expansion/contraction radius in pixels
         :type offset: int [-TILE_SIZE, TILE_SIZE]
         :param feather: the amount to blur the fill, after offset is applied
@@ -1033,20 +1038,28 @@ class Document (object):
         if not self.layer_stack.current.get_fillable():
             make_new_layer = True
         if bbox.empty():
+            xs = map(lambda i: i[0], seeds)
+            ys = map(lambda i: i[1], seeds)
+            min_x = min(xs)
+            max_x = max(xs)
+            min_y = min(ys)
+            max_y = max(ys)
+
             bbox = helpers.Rect()
-            bbox.x = N*int(x//N)
-            bbox.y = N*int(y//N)
-            bbox.w = N
-            bbox.h = N
+            bbox.x = N*int(min_x//N)
+            bbox.y = N*int(min_y//N)
+            bbox.w = N*int(max_x//N) - bbox.x + N
+            bbox.h = N*int(max_y//N) - bbox.y + N
         elif not self.frame_enabled:
-            bbox.expandToIncludePoint(x, y)
+            for (x, y) in seeds:
+                bbox.expandToIncludePoint(x, y)
         if view_bbox:
             view_bbox = helpers.Rect(*view_bbox)
             if bbox.contains(view_bbox):
                 bbox = view_bbox
             elif bbox.overlaps(view_bbox):
                 bbox = bbox.intersection(view_bbox)
-        cmd = command.FloodFill(self, x, y, color, tolerance,
+        cmd = command.FloodFill(self, target_pos, seeds, color, tolerance,
                                 offset, feather, gap_closing_options, mode,
                                 bbox, sample_merged, src_path, make_new_layer)
         self.do(cmd)

lib/floodfill.cpp
@@ -170,6 +170,23 @@ Filler::tile_uniformity(bool empty_tile, PyObject* src_arr)
     return Py_None;
 }
 
+void
+Filler::queue_seeds(
+    PyObject* seeds, PixelBuffer<rgba>& src, PixelBuffer<chan_t> dst)
+{
+    Py_ssize_t num_seeds = PySequence_Size(seeds);
+    for (Py_ssize_t i = 0; i < num_seeds; ++i) {
+        PyObject* seed_tuple = PySequence_GetItem(seeds, i);
+        int x;
+        int y;
+        PyArg_ParseTuple(seed_tuple, "ii", &x, &y);
+        Py_DECREF(seed_tuple);
+        if (!dst(x, y) && pixel_fill_alpha(src(x, y)) > 0) {
+            seed_queue.push(coord(x, y));
+        }
+    }
+}
+
 /*
   Generate and queue pixel coordinates based on a list of ranges, a direction
   of origin indicating the edge the ranges apply to, and source and destination
@@ -274,10 +291,8 @@ Filler::fill(
     // prior to constructing the output seed segment lists
     bool input_seeds[N] = {0,};
 
-    if (PyTuple_CheckExact(seeds)) { // the very first seed (not a range)
-        coord seed_pt;
-        PyArg_ParseTuple(seeds, "ii", &(seed_pt.x), &(seed_pt.y));
-        seed_queue.push(seed_pt);
+    if (seed_origin == edges::none) { // Initial seeds, a list of coordinates
+        queue_seeds(seeds, src, dst);
     } else {
         queue_ranges(seed_origin, seeds, input_seeds, src, dst);
     } // Seed queue populated

lib/floodfill.hpp
@@ -58,6 +58,8 @@ class Filler
     // filled pixel, where an alpha of 0 indicates that the pixel should not be
     // filled (and the fill not propagated through that pixel).
     chan_t pixel_fill_alpha(const rgba& src_px);
+    void queue_seeds(
+        PyObject* seeds, PixelBuffer<rgba>& src, PixelBuffer<chan_t> dst);
     void queue_ranges(
         edge direction, PyObject* seeds, bool marks[N],
         PixelBuffer<rgba>& src, PixelBuffer<chan_t>& dst);

lib/layer/core.py
@@ -682,7 +682,7 @@ class LayerBase (Renderable):
 
     ## Flood fill
 
-    def flood_fill(self, x, y, color, tolerance, offset, feather,
+    def flood_fill(self, target_pos, seeds, color, tolerance, offset, feather,
                    gap_closing_options, mode, framed, bbox, dst_layer=None):
         """Fills a point on the surface with a color
 

lib/layer/data.py
@@ -324,7 +324,7 @@ class SurfaceBackedLayer (core.LayerBase, lib.autosave.Autosaveable):
 
     ## Flood fill
 
-    def flood_fill(self, x, y, color, tolerance, offset, feather,
+    def flood_fill(self, target_pos, seeds, color, tolerance, offset, feather,
                    gap_closing_options, mode, framed, bbox, dst_layer=None):
         """Fills a point on the surface with a color
 
@@ -1194,7 +1194,7 @@ class VectorLayer (FileBackedLayer):
             svg = svg.encode("utf-8")
         file.write(svg)
 
-    def flood_fill(self, x, y, color, tolerance, offset, feather,
+    def flood_fill(self, target_pos, seeds, color, tolerance, offset, feather,
                    gap_closing_options, mode, framed, bbox, dst_layer=None):
         """Fill into dst_layer, with reference to the rasterization of this layer.
         This implementation is virtually identical to the one in LayerStack.
@@ -1211,7 +1211,7 @@ class VectorLayer (FileBackedLayer):
         src = root.get_tile_accessible_layer_rendering(self)
         dst = dst_layer._surface
         tiledsurface.flood_fill(
-            src, x, y, color, tolerance, offset, feather,
+            src, target_pos, seeds, color, tolerance, offset, feather,
             gap_closing_options, mode, framed, bbox, dst
         )
 
@@ -1311,12 +1311,14 @@ class SimplePaintingLayer (SurfaceBackedLayer):
         """True if this layer currently accepts flood fill"""
         return not self.locked
 
-    def flood_fill(self, x, y, color, tolerance, offset, feather,
+    def flood_fill(self, target_pos, seeds, color, tolerance, offset, feather,
                    gap_closing_options, mode, framed, bbox, dst_layer=None):
         """Fills a point on the surface with a color
 
-        :param x: Starting point X coordinate
-        :param y: Starting point Y coordinate
+        :param target_pos: pixel coordinate of target color
+        :type target_pos: tuple
+        :param seeds: set of seed pixel coordinates {(x, y)...}
+        :type seeds: set
         :param color: an RGB color
         :type color: tuple
         :param tolerance: how much filled pixels are permitted to vary
@@ -1348,7 +1350,8 @@ class SimplePaintingLayer (SurfaceBackedLayer):
         if dst_layer is None:
             dst_layer = self
         dst_layer.autosave_dirty = True   # XXX hmm, not working?
-        self._surface.flood_fill(x, y, color, tolerance, offset, feather,
+        self._surface.flood_fill(target_pos, seeds,
+                                 color, tolerance, offset, feather,
                                  gap_closing_options, mode, framed, bbox,
                                  dst_surface=dst_layer._surface)
 

commit e6fedcb01ac39cace2da9850cb82454b6f354763

Fix incorrect initial distances in gc fill seeds
Fixes bug introduced in cb69f75

lib/floodfill.py
@@ -615,7 +615,7 @@ def complement_gc_seeds(seeds, distance_tile):
         complemented_seeds = []
         all_not_max = True
         for (px, py) in seeds:
-            distance = distance_tile[px][py]
+            distance = distance_tile[py][px]
             if distance == INF_DIST:
                 all_not_max = False
             complemented_seeds.append((px, py, distance))